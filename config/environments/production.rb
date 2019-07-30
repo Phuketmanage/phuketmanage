9b90b5dc0c9ef8beeda9fed15512b528b4704d45
Sergey <bytheair@gmail.com>
2019-07-30 21:53:20 +0700

    Set up sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   host = 'phuketmanage.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
