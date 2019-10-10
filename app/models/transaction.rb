7a1a1bd687a10aec9ec8b4db1151f19e388a3e15
Sergey <bytheair@gmail.com>
2019-10-10 12:21:29 +0700

    Tune transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -21,37 +21,38 @@ class Transaction < ApplicationRecord
     balances.destroy_all if balances.any?
     if types1.include?(type)
       errors.add(:base, 'Need to select owner or house') and return if house_id.nil? || user_id.nil?
-      return if errors.any?
       balance_outs.create!(debit: de_ow, credit: de_co)
       balances.create!(debit: de_co)
     elsif types2.include?(type)
       errors.add(:base, 'Amount can not be blank') if cr_ow.nil? || cr_ow == 0
       errors.add(:base, 'Need to select owner or house') if house_id.nil? && user_id.nil?
-      errors.add(:base, 'Need to select house') and return if user_id.nil?
+      errors.add(:base, 'Need to select house') if user_id.nil?
       return if errors.any?
       balance_outs.create!(credit: cr_ow)
       balances.create!(debit: cr_ow)
     elsif types3.include?(type)
       errors.add(:base, 'Need to select owner or house') and return if house_id.nil? && user_id.nil?
-      return if errors.any?
+      errors.add(:base, 'Amount can not be blank') and return if de_ow == 0
       balance_outs.create!(debit: de_ow)
     elsif types4.include?(type)
       errors.add(:base, 'Need to select house') and return if user_id.nil?
       errors.add(:base, 'Need to select house') and return if house_id.nil?
-      return if errors.any?
+      errors.add(:base, 'Amount can not be blank') and return if cr_ow == 0 && cr_co == 0 && de_co == 0
       balance_outs.create!(credit: cr_ow + cr_co + de_co)
       balances.create!(debit: cr_co + de_co, credit: cr_co)
     elsif types5.include?(type)
       errors.add(:base, 'Need to select house') and return if house_id.nil?
-      return if errors.any?
+      errors.add(:base, 'Amount can not be blank') and return if cr_ow == 0
       balance_outs.create!(credit: cr_ow) if cr_ow > 0
     elsif types6.include?(type)
+      errors.add(:base, 'Amount can not be blank') and return if cr_co == 0
       balances.create!(credit: cr_co) if cr_co > 0
     elsif types7.include?(type)
+      errors.add(:base, 'Amount can not be blank') and return if de_ow == 0 && cr_ow == 0 && de_co == 0 && cr_co == 0
       if de_ow > 0 || cr_ow > 0
-      errors.add(:base, 'Need to select owner or house') and return if house_id.nil? || user_id.nil?
-      return if errors.any?
-      balance_outs.create!(debit: de_ow, credit: cr_ow)
+        errors.add(:base, 'Need to select owner or house') and return if house_id.nil? && user_id.nil?
+        return if errors.any?
+        balance_outs.create!(debit: de_ow, credit: cr_ow)
       end
       balances.create!(debit: de_co, credit: cr_co) if de_co > 0 || cr_co > 0
     else
