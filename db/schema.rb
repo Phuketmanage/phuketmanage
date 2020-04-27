99b6f2a5c761a8ca7b0f7b181bb330d778225a6c
Sergey <bytheair@gmail.com>
2020-04-27 11:15:39 +0700

    Fix timeline and remove old migrations for ref_no

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_093525) do
+ActiveRecord::Schema.define(version: 2020_04_26_093459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,6 @@ ActiveRecord::Schema.define(version: 2020_04_26_093525) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "ref_no"
-    t.string "ref_no_iv"
-    t.string "ref_no_re"
     t.index ["transaction_id"], name: "index_balances_on_transaction_id"
   end
 
