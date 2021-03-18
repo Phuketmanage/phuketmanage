06a9e8cd9bbceb84605819fdcc6eb0ba98e4f69b
Sergey <bytheair@gmail.com>
2021-03-18 11:58:31 +0700

    Tune last

--- app/controllers/water_usages_controller.rb
+++ app/controllers/water_usages_controller.rb
@@ -9,8 +9,7 @@ class WaterUsagesController < ApplicationController
   # GET /water_usages.json
   def index
     if params[:house_id].present?
-      @water_usage = WaterUsage.where(house_id: params[:house_id]).order(date: :desc).last(30)
-
+      @water_usage = WaterUsage.where(house_id: params[:house_id]).order(date: :desc).first(30)
     else
       @water_usages = WaterUsage.all
       @house_groups = HouseGroup.all
