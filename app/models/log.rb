92c9446e80a74a77bc2194270aefcbcb3f414838
Vasilii Sergeev <24823177+VaSSa1568@users.noreply.github.com>
2023-05-15 10:47:00 +0300

    Log table Table was modified - column when  removed - column action renamed to what - added column with
    Closes #83

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,13 +1,13 @@
 class Log < ApplicationRecord
   the_schema_is "logs" do |t|
-    t.datetime "when"
     t.string "where"
     t.text "before"
     t.text "after"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "who"
-    t.string "action"
+    t.string "what"
+    t.string "with"
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_18_173347) do
+ActiveRecord::Schema.define(version: 2023_05_15_073619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -305,14 +305,14 @@ ActiveRecord::Schema.define(version: 2022_06_18_173347) do
   end
 
   create_table "logs", force: :cascade do |t|
-    t.datetime "when"
     t.string "where"
     t.text "before"
     t.text "after"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "who"
-    t.string "action"
+    t.string "what"
+    t.string "with"
   end
 
   create_table "notifications", force: :cascade do |t|
