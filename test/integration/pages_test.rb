11f6bb8aa9068dfe7c8c9fbc9c714374baea7fea
Vasilii Sergeev <24823177+VaSSa1568@users.noreply.github.com>
2023-05-15 10:30:56 +0300

    Dashboard link - link added to admin navbar - tests were added
    Closes #82

---
+++ test/integration/pages_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class PagesTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "should show dashboard link" do
+    get root_path
+    assert_response :success
+    assert_no_match "Dashboard", response.body
+    sign_in users(:admin)
+    get root_path
+    assert_response :success
+    assert_match "Dashboard", response.body
+    sign_out users(:admin)
+    sign_in users(:manager)
+    get root_path
+    assert_response :success
+    assert_match "Dashboard", response.body
+    sign_out users(:manager)
+    sign_in users(:accounting)
+    get root_path
+    assert_response :success
+    assert_match "Dashboard", response.body
+    sign_out users(:accounting)
+    sign_in users(:owner)
+    get root_path
+    assert_response :success
+    assert_no_match "Dashboard", response.body
+    sign_out users(:owner)
+  end
+end
