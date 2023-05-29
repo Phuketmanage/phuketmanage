83c35d907f748f39517d970f7b33bd3a03dd84a6
Vasilii Sergeev <24823177+VaSSa1568@users.noreply.github.com>
2023-05-29 17:12:09 +0300

    Scopes for active and inactive owners - scopes active_owners and inactive_owners were added - tests were added
    Closes #96

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'User', :type => :model do
+  it "scope should return proper records" do
+    expect do 
+      create_list(:user, 3, :owner, balance_closed: true)
+    end.to change {User.inactive_owners.count}.by(3)
+    expect do 
+      create_list(:user, 5, :owner)
+    end.to change {User.active_owners.count}.by(5)
+  end
+end
