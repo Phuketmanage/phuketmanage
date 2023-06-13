06473fda0e1e97998dbb5a4693103558ff72ed2c
150Years <bytheair@gmail.com>
2023-06-14 00:46:06 +0700

    #167-reports

---
+++ spec/requests/reports_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+  describe "GET /index" do
+    # let(:admin) { create(:user, :admin)}
+    # it "returns http success" do
+    #   sign_in admin
+    #   get "/reports"
+    #   expect(response).to have_http_status(:success)
+    # end
+  end
