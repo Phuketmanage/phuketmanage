267dcf674ee27a422facb5f4c0f34afa2c7ead0b
Vasilii Sergeev <24823177+VaSSa1568@users.noreply.github.com>
2023-06-30 14:05:48 +0300

    House tests - factory house_type were added - tests for proper show for_rent, not_for_rent and inactive houses were added
    Closes #184

--- spec/factories/house_type_factory.rb
+++ spec/factories/house_type_factory.rb
@@ -1,7 +1,22 @@
 FactoryBot.define do
   factory :house_type do
-    name_en { Faker::Address.community }
-    name_ru { Faker::Address.community }
+    name_en { "House" }
+    name_ru { "Дом" }
     comm { Faker::Number.number(digits: 3) }
+
+    trait :villa do
+      name_en { "Villa" }
+      name_ru { "Вилла" }
+    end
+
+    trait :townhouse do
+      name_en { "Townhouse" }
+      name_ru { "Таунхаус" }
+    end
+
+    trait :appartment do
+      name_en { "Appartment" }
+      name_ru { "Апартаменты" }
+    end
   end
 end
--- spec/system/house_spec.rb
+++ spec/system/house_spec.rb
@@ -3,44 +3,67 @@ require 'rails_helper'
 describe 'House' do
   let(:admin) { create(:user, :admin) }
   let(:manager) { create(:user, :manager) }
-  let!(:house) { create(:house) }
+  let(:balance_closed) { false }
+  let(:unavailable) { false }
+  let!(:house) { create :house, unavailable: unavailable, balance_closed: balance_closed }
+  subject { page }
 
-  context "when manager" do
-    before { sign_in manager }
+  describe "Access" do
 
-    it "inactive shouldn't be accesed from manager" do
-      visit houses_inactive_path
-      expect(page).to have_content 'You are not authorized to access this page.'
+    context "when manager" do
+      before { sign_in manager }
+
+      context "when visiting houses" do
+        before { visit houses_path }
+        it { is_expected.to_not have_link 'Destroy' }
+        it { is_expected.to_not have_link 'Inactive Houses' }
+      end
+
+      context "when visiting inactive houses" do
+        before { visit houses_inactive_path }
+        it { is_expected.to have_content 'You are not authorized to access this page.' }
+      end
     end
 
-    it "shouldn't have Destroy link for other roles" do
-      visit houses_path
-      expect(page).to have_no_link 'Destroy'
+    context "when admin" do
+      before { sign_in admin }
+
+      context "when visiting houses" do
+        before { visit houses_path }
+        it { is_expected.to have_link 'Destroy' }
+        it { is_expected.to have_link 'Inactive Houses' }
+      end
+
+      context "when visiting inactive houses" do
+        before { visit houses_inactive_path }
+        it { is_expected.to have_content 'Inactive Houses' }
+      end
     end
 
-    it "shouldn't have Inactive button for other roles" do
-      visit houses_path
-      expect(page).to have_no_link 'Inactive Houses'
-    end
-  end
+    describe "listing" do
+      before { sign_in admin }
 
-  context "when admin" do
-    before { sign_in admin }
+      describe '#unavailable' do
+        before { visit houses_path}
 
-    it "inactive should be accessed by admin" do
-      visit houses_inactive_path
-      expect(page).to have_content 'Inactive Houses'
-    end
+        context 'when house is for rent' do
+          let(:unavailable) { false }
+          it { is_expected.to have_selector('.for_rent .house', text: house.title_en) }
+        end
 
-    it "should have Inactive button for admin" do
-      visit houses_path
-      expect(page).to have_link 'Inactive Houses'
-    end
+        context 'when house is not for rent' do
+          let(:unavailable) { true }
+          it { is_expected.to have_selector('.for_rent .house', text: house.title_en) }
+        end
+      end
 
-    it "should have Destroy link for admin" do
-      sign_in admin
-      visit houses_path
-      expect(page).to have_link 'Destroy'
+      describe '#balance_closed' do
+        before { visit houses_inactive_path }
+        context 'when house is inactive' do
+          let(:balance_closed) { true }
+          it { is_expected.to have_selector('.inactive_houses .house', text: house.title_en) }
+        end
+      end
     end
   end
 end
