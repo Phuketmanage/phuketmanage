b6643f4f6f01b76dd8905a5a1561f92416af16ae
Nick Ostrovsky <nick@firedev.com>
2023-07-01 07:33:02 +0300

    fix(Spec): Fix House System Spec

--- spec/system/house_spec.rb
+++ spec/system/house_spec.rb
@@ -1,43 +1,46 @@
 require 'rails_helper'
 
 describe 'House' do
-  let(:admin) { create(:user, :admin)}
-  let(:manager) {create(:user, :manager)}
+  let(:admin) { create(:user, :admin) }
+  let(:manager) { create(:user, :manager) }
+  let!(:house) { create(:house) }
 
-  it "inactive should be accesed by admin" do
-    sign_in admin
-    visit houses_inactive_path
-    expect(page).to have_content 'Inactive Houses'
+  context "when manager" do
+    before { sign_in manager }
+
+    it "inactive shouldn't be accesed from manager" do
+      visit houses_inactive_path
+      expect(page).to have_content 'You are not authorized to access this page.'
+    end
+
+    it "shouldn't have Destroy link for other roles" do
+      visit houses_path
+      expect(page).to have_no_link 'Destroy'
+    end
+
+    it "shouldn't have Inactive button for other roles" do
+      visit houses_path
+      expect(page).to have_no_link 'Inactive Houses'
+    end
   end
 
-  it "inactive shouldn't be accesed from manager" do
-    sign_in manager
-    visit houses_inactive_path
-    expect(page).to have_content 'You are not authorized to access this page.'
-  end
+  context "when admin" do
+    before { sign_in admin }
 
-  it "should have Inactive button for admin" do
-    sign_in admin
-    visit houses_path
-    expect(page).to have_link 'Inactive Houses'
-  end
+    it "inactive should be accessed by admin" do
+      visit houses_inactive_path
+      expect(page).to have_content 'Inactive Houses'
+    end
 
-  it "shouldn't have Inactive button for other roles" do
-    sign_in manager
-    visit houses_path
-    expect(page).to have_no_link 'Inactive Houses'
-  end
+    it "should have Inactive button for admin" do
+      visit houses_path
+      expect(page).to have_link 'Inactive Houses'
+    end
 
-  it "should have Destroy link for admin" do
-    sign_in admin
-    visit houses_path
-    expect(page).to have_link 'Destroy'
+    it "should have Destroy link for admin" do
+      sign_in admin
+      visit houses_path
+      expect(page).to have_link 'Destroy'
+    end
   end
-
-  it "shouldn't have Destroy link for other roles" do
-    sign_in manager
-    visit houses_path
-    expect(page).to have_no_link 'Destroy'
-  end
-
 end
