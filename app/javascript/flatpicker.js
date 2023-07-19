4e595253c83d24255742171717b1e46796e3373a
Vasilii Sergeev <24823177+VaSSa1568@users.noreply.github.com>
2023-07-19 17:25:03 +0300

    Localization for flatpicker - flatpicker opts refactored - added Russian language to flatpickr - search forms updated with various data
    Closes #243

--- app/javascript/flatpicker.js
+++ app/javascript/flatpicker.js
@@ -1,12 +1,17 @@
 import flatpickr from "flatpickr"
+import { Russian } from "flatpickr/dist/l10n/ru.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   rp_elements = document.querySelectorAll('.js_rangepicker')
   if (rp_elements) {
-    occupiedDays = ''
+    element_opts = {}
     for (elem of rp_elements){
-      if (elem.dataset && elem.dataset.occupiedDays) {occupiedDays = JSON.parse(elem.dataset.occupiedDays)}
-      opts = { altInput: true, altFormat: "d.m.Y", dateFormat: "Y-m-d", mode: "range"}
+      if (elem.dataset) { 
+        element_opts = JSON.stringify(elem.dataset)
+        elem.dataset.locale == 'ru' ?  element_opts.locale = Russian : delete element_opts.locale
+      }
+      default_opts = { altInput: true, altFormat: "d.m.Y", dateFormat: "Y-m-d", mode: "range" }
+      opts = { ...default_opts, ...element_opts }
       picker = flatpickr(elem, opts)
     }
   }
