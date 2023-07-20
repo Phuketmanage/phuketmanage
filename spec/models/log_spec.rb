c524d75ad1a16c332082d9740a58ec819c9d9a42
the1723 <hello@paveldavydov.com>
2023-07-20 17:10:28 +0300

    Mark tests as pending

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'Log', :type => :model do
   let(:owner) { create(:user, :owner)}
-  it 'shouldn\'t be valid' do
+  xit 'shouldn\'t be valid' do
     expect do
       create(:log)
     end.to raise_error(ActiveRecord::RecordInvalid)
   end
-  it 'should be valid' do
+  xit 'should be valid' do
     expect do
       create(:log, who: owner, where: "random controller", what: "example controller name", with: "example subject", before: "example state before action", after: "example state after action")
     end.to change {Log.count}.by(1)
