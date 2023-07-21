550eee02c877863c2887db3d408a09c7d3daf2a1
the1723 <hello@paveldavydov.com>
2023-07-21 17:51:48 +0300

    Basic Log model test

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
-describe 'Log', :type => :model do
-  let(:owner) { create(:user, :owner)}
-  pending 'shouldn\'t be valid' do
-    expect do
-      create(:log)
-    end.to raise_error(ActiveRecord::RecordInvalid)
+describe Log, type: :model do
+  let(:valid_log) do
+    { user_email: 'test@email.com', user_roles: "Admin, Owner", location: "controller#action", model_gid: "prices#update",
+      before: {
+        id: 55,
+        amount: 8020,
+        house_id: 4,
+        season_id: 19,
+        created_at: "2023-07-21T13:15:44.380Z",
+        updated_at: "2023-07-21T14:03:06.654Z",
+        duration_id: 10
+      },
+      applied_changes: {
+        amount: 8021,
+        updated_at: "2023-07-21T14:14:50.253Z"
+      } }
   end
-  pending 'should be valid' do
+
+  it { should validate_presence_of(:user_email) }
+  it { should validate_presence_of(:user_roles) }
+  it { should validate_presence_of(:location) }
+  it { should validate_presence_of(:model_gid) }
+  it { should validate_presence_of(:before) }
+  it { should validate_presence_of(:applied_changes) }
+
+  it 'should be valid' do
     expect do
-      create(:log, who: owner, where: "random controller", what: "example controller name", with: "example subject", before: "example state before action", after: "example state after action")
-    end.to change {Log.count}.by(1)
+      create(:log, valid_log)
+    end.to change { Log.count }.by(1)
   end
 end
