e1bd09f51f1a53a1e1da5b7ae4ea9161fae2efed
the1723 <hello@paveldavydov.com>
2023-07-21 15:22:35 +0300

    Add Selenium with chrome

--- spec/support/capybara_config.rb
+++ spec/support/capybara_config.rb
@@ -1,5 +1,16 @@
 RSpec.configure do |config|
+  # Default driver without js support for faster tests
   config.before(:each, type: :system) do
     driven_by :rack_test
+  end
+  # Use Chrome headless to test js
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+    Capybara.page.current_window.resize_to(1024, 768)
+  end
+  # Use Chrome to open browser window
+  config.before(:each, type: :system, js_visual: true) do
+    driven_by :selenium_chrome
+    Capybara.page.current_window.resize_to(1024, 768)
   end
 end
