cd7880594aa8d36bdcfeab9e1aae7d06e863889d
the1723 <hello@paveldavydov.com>
2023-07-23 20:42:03 +0300

    Loggable concern and specs

---
+++ app/controllers/concerns/loggable.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+## Method to log events inside controller. Automatically logs user, controller and action name, subject model GID,
+# subject before state and applyed changes. If no changes were made during update, no log will be created.
+#
+## Accepts:
+# subject - model instance object
+#
+## Returns:
+# - True if the action was successful
+# - False if action failed.
+#
+# If there was failure it sends error message to Rails error log.
+#
+## Usage
+#
+# 1. Enable global usage in settings ['user_activity_logging'] == 'true'
+#
+# 2. Add in controller:
+# include Loggable
+#
+# 3. When you want to log action, call log_event(subject) after update:
+#
+# if price.update(price_params)
+#   log_event(price)
+#   do_some_staff
+# end
+#
+module Loggable
+  extend ActiveSupport::Concern
+
+  included do
+    def log_event(subject)
+      @subject = subject
+
+      return true unless logging_enabled? && changes_applied?
+
+      log = Log.new(user_email: actor_email, user_roles: actor_roles, location: action_location,
+                    model_gid: subject_gid, before_values: previous_state, applied_changes: new_values)
+      if log.save
+        true
+      else
+        logger.error "Error logging action at #{action_location}. Reasons: #{log.errors.full_messages.join('; ')}"
+        false
+      end
+    end
+
+    private
+
+    def logging_enabled?
+      @settings.key?("user_activity_logging_enabled")
+    end
+
+    # Check for situation when update is done with no changed attributes
+    def changes_applied?
+      @subject.previous_changes.any?
+    end
+
+    def actor_email
+      if current_user
+        current_user.email
+      else
+        'Unauthorized'
+      end
+    end
+
+    def actor_roles
+      if current_user
+        current_user.roles.pluck(:name).join(', ').presence || ["Unauthorized"]
+      else
+        ["Unauthorized"]
+      end
+    end
+
+    def action_location
+      "#{controller_name}##{action_name}"
+    end
+
+    def new_values
+      @subject.previous_changes.transform_values(&:last)
+    end
+
+    def old_values
+      @subject.previous_changes.transform_values(&:first)
+    end
+
+    def previous_state
+      @subject.as_json.merge(old_values)
+    end
+
+    def subject_gid
+      @subject.to_global_id
+    end
+  end
+end
--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -2,16 +2,46 @@
 
 require 'rails_helper'
 
-describe 'Log', :type => :model do
-  let(:owner) { create(:user, :owner)}
-  it 'shouldn\'t be valid' do
-    expect do
-      create(:log)
-    end.to raise_error(ActiveRecord::RecordInvalid)
+describe Log do
+  let(:log) { build(:log) }
+
+  it "is valid with valid attributes" do
+    expect(log).to be_valid
   end
-  it 'should be valid' do
+
+  it "is not valid without a user_email" do
+    log.user_email = nil
+    expect(log).to be_invalid
+  end
+
+  it "is not valid without a user_roles" do
+    log.user_roles = nil
+    expect(log).to be_invalid
+  end
+
+  it "is not valid without a location" do
+    log.location = nil
+    expect(log).to be_invalid
+  end
+
+  it "is not valid without a model_gid" do
+    log.model_gid = nil
+    expect(log).to be_invalid
+  end
+
+  it "is not valid without a before_values" do
+    log.before_values = nil
+    expect(log).to be_invalid
+  end
+
+  it "is not valid without a applied_changes" do
+    log.applied_changes = nil
+    expect(log).to be_invalid
+  end
+
+  it 'is valid' do
     expect do
       log.save
-    end.to change { Log.count }.by(1)
+    end.to change(described_class, :count).by(1)
   end
 end
