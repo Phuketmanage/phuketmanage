5845c4c7937e21bf0222034581b35c5da2ac6c7c
the1723 <hello@paveldavydov.com>
2023-07-24 17:18:09 +0300

    With workers: :number_of_processors

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,7 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   # Parallel Testing with Processes
-  # parallelize(workers: :number_of_processors)
+  parallelize(workers: :number_of_processors)
 end
 
 module ActionDispatch::Integration
