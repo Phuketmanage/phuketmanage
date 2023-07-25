5cf3a894bd20c7f3b35d3b8b20298f8989aa55ad
the1723 <hello@paveldavydov.com>
2023-07-25 15:58:06 +0300

    Moved imported controllers to application.js to prevent 'rails generate stimulus' to remove them Created hotkeys_actions controller to hold methods

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,14 @@
 import { Application } from "@hotwired/stimulus"
+import Hotkeys from 'stimulus-hotkeys'
+import Reveal from 'stimulus-reveal-controller'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+application.register('reveal', Reveal)
+application.register('hotkeys', Hotkeys)
 
 export { application }
