baf5e7441aa54e10e6a69bb516cc363f5cefda6a
the1723 <hello@paveldavydov.com>
2023-07-25 15:59:14 +0300

    Add test no check 'a' without 'shift' click

--- spec/system/hotkeys_spec.rb
+++ spec/system/hotkeys_spec.rb
@@ -11,10 +11,9 @@ describe 'Hotkeys' do
   describe 'Transaction index page', js: true do
     let(:start_page) { transactions_path }
     let(:desired_page) { new_transaction_path(locale: :en) }
+    let(:keys) { [:shift, 'a'] }
 
     context 'when pressed Shift+A' do
-      let(:keys) { [:shift, 'a'] }
-
       context 'when admin' do
         it "opens new transaction page" do
           sign_in admin
@@ -40,6 +39,17 @@ describe 'Hotkeys' do
           page.send_keys keys
           expect(page).to have_current_path(desired_page)
         end
+      end
+    end
+
+    context 'when only A pressed' do
+      let(:keys) { ['a'] }
+
+      it "doese nothing" do
+        sign_in admin
+        visit start_page
+        page.send_keys keys
+        expect(page).to have_current_path(start_page)
       end
     end
   end
