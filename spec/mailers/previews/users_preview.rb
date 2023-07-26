ffdad4c335854e4271082caa7ec291dc8000255c
the1723 <hello@paveldavydov.com>
2023-07-26 15:35:41 +0300

    User reset email specs and preview Moved faker gem also to dev env to be able to use Preview: http://localhost:3000/rails/mailers

---
+++ spec/mailers/previews/users_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/users
+class UsersPreview < ActionMailer::Preview
+  def reset_password_instructions
+    user = FactoryBot.create(:user, :admin)
+    user.update(locale: params[:locale])
+    token = user.send(:set_reset_password_token)
+    UsersMailer.reset_password_instructions(user, token)
+  end
+end
