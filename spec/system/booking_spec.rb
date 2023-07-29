2bee638ec76fb555fcf439a9378c32d1394553a8
the1723 <hello@paveldavydov.com>
2023-07-29 15:38:37 +0300

    test(booking): complete refactoring

--- spec/system/booking_spec.rb
+++ spec/system/booking_spec.rb
@@ -4,64 +4,105 @@
 
 require 'rails_helper'
 
-describe 'Booking', js: true do
-  let(:owner) { create(:user, :owner) }
-  # Houses for owner one
-  let(:house_one) { create(:house, :with_seasons, owner:) }
-  let(:house_two) { create(:house, :with_seasons, owner:) }
-  # Dates
-  let(:current_date) { Time.zone.today }
-  let(:date_start) { current_date + 2.days }
-  let(:date_finish) { current_date + 10.days }
+describe 'Booking' do
+  subject { page }
 
-  before do
-    # Bookings for owner
-    create(:booking, house: house_one, start: date_start, finish: date_finish, status: "pending")
-    create(:booking, house: house_two, start: date_start, finish: date_finish + 1.week, status: "pending")
-    # Other owners bookings
-    2.times do
-      create(:booking, house: create(:house, :with_seasons), start: date_start, finish: date_finish, status: "pending")
-    end
-  end
+  let(:current_date) { Time.zone.today }
+  let(:date_start) { current_date }
+  let(:date_finish) { current_date + 10.days }
 
   context 'when user is admin' do
     login_admin
 
-    it "shows bookings with status not canceled" do
-      visit "/bookings?from=#{current_date}&to=#{current_date + 2.years}"
-      # Count by rows in table: 1 record produce 2 rows and 1 extra from table header
-      expect(page).to have_selector('table tr', count: 9)
+    let!(:booking_pending) do
+      create(:booking, house: create(:house, :with_seasons), start: date_start, finish: date_finish,
+                       status: :pending)
+    end
+    let!(:booking_canceled) do
+      create(:booking, house: create(:house, :with_seasons), start: date_start, finish: date_finish,
+                       status: :canceled)
     end
 
-    it "shows bookings with status canceled" do
-      create(:booking, house: house_one, start: date_start, finish: date_finish, status: "canceled")
-      visit "/bookings/canceled?from=#{current_date}&to=#{current_date + 2.years}"
-      # Count by rows in table: 1 record produce 2 rows and 1 extra from table header
-      expect(page).to have_selector('table tr', count: 3)
+    context "when visiting bookings" do
+      before do
+        visit "/bookings?from=#{current_date}&to=#{current_date + 2.years}"
+      end
+
+      it { is_expected.to have_text("Bookings (1)") }
+      it { is_expected.to have_selector('td', text: booking_pending.house.code) }
+      it { is_expected.not_to have_selector('td', text: booking_canceled.house.code) }
     end
 
-    it "shows pending on dashboard view" do
-      visit dashboard_path
-      expect(page).to have_selector('div.pending_booking', count: 4)
+    context "when visiting canceled bookings" do
+      before do
+        visit "/bookings/canceled?from=#{current_date}&to=#{current_date + 2.years}"
+      end
+
+      it { is_expected.to have_text("Canceled bookings (1)") }
+      it { is_expected.to have_selector('td', text: booking_canceled.house.code) }
+      it { is_expected.not_to have_selector('td', text: booking_pending.house.code) }
+    end
+
+    context "when visiting dashboard view" do
+      before { visit dashboard_path }
+
+      it { is_expected.to have_text("Pending Bookings") }
+      it { is_expected.to have_selector('b', text: booking_pending.house.title_en, count: 1) }
+      it { is_expected.to have_selector("div[data-id='#{booking_pending.id}']", count: 1) }
+      it { is_expected.not_to have_selector("div[data-id='#{booking_canceled.id}']") }
     end
   end
 
   context 'when user is owner' do
+    let(:owner_one) { create(:user, :owner) }
+    let(:owner_two) { create(:user, :owner) }
+
+    let(:house_one) { create(:house, :with_seasons, owner: owner_one) }
+    let(:house_two) { create(:house, :with_seasons, owner: owner_one) }
+    let(:house_three) { create(:house, :with_seasons, owner: owner_two) }
+
     before do
-      sign_in owner
-      visit "/owner/bookings?from=#{current_date}&to=#{current_date + 2.years}&commit=Show"
+      create(:booking, house: house_one, start: date_start, finish: date_finish,
+                       sale: 10_000, agent: 0, comm: 2_000, nett: 8_000, status: :pending)
+      create(:booking, house: house_two, start: date_start, finish: date_finish + 2.days,
+                       sale: 12_000, agent: 0, comm: 2_400, nett: 9_600, status: :confirmed)
+      create(:booking, house: house_three, start: date_start, finish: date_finish,
+                       sale: 20_000, agent: 2_000, comm: 2_000, nett: 16_000, status: :pending)
+      create(:booking, house: house_three, start: date_start + 12.days, finish: date_finish + 14.days,
+                       sale: 30_000, agent: 0, comm: 6_000, nett: 24_000, status: :pending)
     end
 
-    it "shows correct title" do
-      expect(page).to have_text("Bookings (2)")
+    context "when owner_one" do
+      before do
+        sign_in owner_one
+        visit "/owner/bookings?from=#{current_date}&to=#{current_date + 2.years}&commit=Show"
+      end
+
+      it { is_expected.to have_text("Bookings (2)") }
+      it { is_expected.to have_selector('tr.booking_row', count: 2) }
+      it { is_expected.to have_selector('td', text: house_one.code) }
+      it { is_expected.to have_selector('td', text: house_two.code) }
+      it { is_expected.not_to have_selector('td', text: house_three.code) }
+      it { is_expected.to have_selector('td', text: '10') }
+      it { is_expected.to have_selector('td', text: '12') }
+      it { is_expected.to have_selector('td', text: '8,000') }
+      it { is_expected.to have_selector('td', text: '9,600') }
     end
 
-    it "shows table wit two rows" do
-      expect(page.all('tr.booking_row').count).to eq(2)
-    end
+    context "when owner_two" do
+      before do
+        sign_in owner_two
+        visit "/owner/bookings?from=#{current_date}&to=#{current_date + 2.years}&commit=Show"
+      end
 
-    it "shows ony bookings for his houses" do
-      expect(page.text).to include house_one.code, house_two.code
+      it { is_expected.to have_text("Bookings (2)") }
+      it { is_expected.to have_selector('tr.booking_row', count: 2) }
+      it { is_expected.not_to have_selector('td', text: house_one.code) }
+      it { is_expected.not_to have_selector('td', text: house_two.code) }
+      it { is_expected.to have_selector('td', text: '10') }
+      it { is_expected.to have_selector('td', text: '12') }
+      it { is_expected.to have_selector('td', text: '16,000') }
+      it { is_expected.to have_selector('td', text: '24,000') }
     end
   end
 end
