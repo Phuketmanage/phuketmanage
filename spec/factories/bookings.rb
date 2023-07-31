984981eeb8080c37d04d70f997a23b48ad2a6c79
Vasilii Sergeev <24823177+VaSSa1568@users.noreply.github.com>
2023-07-31 15:13:29 +0300

    fix(booking_factory): add random generation for fields
    Closes #324

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -3,10 +3,10 @@
 FactoryBot.define do
   factory :booking do
     client_details { Faker::Name.last_name }
-    sale { 1000 }
-    agent { 0 }
-    comm { 0 }
-    nett { 1000 }
+    sale { Random.rand(1..3) * 1000 }
+    agent { Random.rand(0..5) * 100 }
+    comm { Random.rand(1..6) * 100 }
+    nett { sale - agent - comm }
     allotment { false }
   end
 end
