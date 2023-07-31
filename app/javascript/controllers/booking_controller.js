07fab79651b507549b2ffc6b0d2eac87f20c49bf
the1723 <hello@paveldavydov.com>
2023-07-31 23:18:54 +0300

    fix: price fields actions logic refactor: booking stimulus methods

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -13,79 +13,67 @@ export default class extends Controller {
   ]
 
   connect() {
-    this.updateAgentPercentage()
-    this.updateCommissionPercentage()
+    this.updatePercentage()
   }
 
-  calculateAgentPercent(event) {
+  calculateTwentyPercent(event) {
     event.preventDefault()
 
-    const saleValue = Number(this.saleFieldTarget.value)
-    const commValue = Number(this.commFieldTarget.value)
+    const saleValue = parseInt(this.saleFieldTarget.value)
 
-    const agentValue = saleValue * 0.1
-    this.agentFieldTarget.value = agentValue.toFixed(0)
+    const commValue = saleValue * 0.2
 
-    const updatedCommValue = commValue - agentValue
-    this.commFieldTarget.value = updatedCommValue.toFixed(0)
+    this.commFieldTarget.value = commValue.toFixed(0)
+    this.agentFieldTarget.value = 0
 
     this.updateNettField()
-    this.updateAgentPercentage()
-    this.updateCommissionPercentage()
+    this.updatePercentage()
+  }
+
+  calculateTenPercent(event) {
+    event.preventDefault()
+
+    const saleValue = parseInt(this.saleFieldTarget.value)
+
+    const commValue = saleValue * 0.1
+
+    this.commFieldTarget.value = commValue.toFixed(0)
+    this.agentFieldTarget.value = 0
+
+    this.updateNettField()
+    this.updatePercentage()
   }
 
   calculateFiftyFifty(event) {
     event.preventDefault()
 
-    const commValue = Number(this.commFieldTarget.value)
+    const commValue = parseInt(this.commFieldTarget.value)
 
     const halfValue = commValue * 0.5
     this.agentFieldTarget.value = halfValue.toFixed(0)
-    this.commFieldTarget.value = halfValue.toFixed(0)
 
     this.updateNettField()
-    this.updateAgentPercentage()
-    this.updateCommissionPercentage()
-  }
-
-  calculateCommPercent(event) {
-    event.preventDefault()
-
-    const saleValue = Number(this.saleFieldTarget.value)
-
-    const commValue = saleValue * 0.2
-
-    this.commFieldTarget.value = commValue.toFixed(0)
-
-    this.updateNettField()
-    this.updateAgentPercentage()
-    this.updateCommissionPercentage()
+    this.updatePercentage()
   }
 
   updateNettField() {
-    const saleValue = Number(this.saleFieldTarget.value)
-    const agentValue = Number(this.agentFieldTarget.value)
-    const commValue = Number(this.commFieldTarget.value)
+    const saleValue = parseInt(this.saleFieldTarget.value)
+    const agentValue = parseInt(this.agentFieldTarget.value)
+    const commValue = parseInt(this.commFieldTarget.value)
 
     const nettValue = saleValue - agentValue - commValue
     this.nettFieldTarget.value = nettValue.toFixed(0)
   }
 
-  updateAgentPercentage() {
-    const saleValue = Number(this.saleFieldTarget.value)
-    const agentValue = Number(this.agentFieldTarget.value)
+  updatePercentage() {
+    const saleValue = parseInt(this.saleFieldTarget.value)
+    const agentValue = parseInt(this.agentFieldTarget.value)
+    const commValue = parseInt(this.commFieldTarget.value)
 
     const bookingAgentPercentage = (agentValue / saleValue) * 100
+    const bookingCommissionPercentage = (commValue / saleValue) * 100
 
     this.agentPercentageTarget.textContent = bookingAgentPercentage.toFixed(0) + '%'
-  }
-
-  updateCommissionPercentage() {
-    const saleValue = Number(this.saleFieldTarget.value)
-    const agentValue = Number(this.commFieldTarget.value)
-
-    const bookingCommissionPercentage = (agentValue / saleValue) * 100
-
     this.commPercentageTarget.textContent = bookingCommissionPercentage.toFixed(0) + '%'
   }
 }
