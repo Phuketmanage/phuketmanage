087dd74f35abca4b8f380cb384dfaf4a570050ae
Pavel Davydov <69132749+the1723@users.noreply.github.com>
2023-08-01 19:48:02 +0300

    Bookings calculate price stimulus controller and actions (#334)
    * feat(booking): calculate agent and comm percentage
    * feat(booking): quick actions
    * chore(booking): moved to stimulus

---
+++ app/javascript/controllers/booking_controller.js
@@ -0,0 +1,68 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [
+    "saleField",
+    "agentField",
+    "commField",
+    "nettField",
+    "agentTenPercentLink",
+    "fiftyFiftyLink",
+    "agentPercentage",
+    "commPercentage"
+  ]
+
+  connect() {
+    this.updatePercentage()
+  }
+
+  calculateTwentyPercent() {
+    const saleValue = parseInt(this.saleFieldTarget.value)
+
+    const commValue = saleValue * 0.2
+
+    this.commFieldTarget.value = commValue.toFixed(0)
+    this.agentFieldTarget.value = 0
+
+    this.updatePercentage()
+  }
+
+  calculateTenPercent() {
+    const saleValue = Number(this.saleFieldTarget.value)
+
+    const commValue = saleValue * 0.1
+
+    this.commFieldTarget.value = commValue.toFixed(0)
+    this.agentFieldTarget.value = 0
+
+    this.updatePercentage()
+  }
+
+  calculateFiftyFifty() {
+    const commValue = Number(this.commFieldTarget.value)
+
+    const halfValue = commValue * 0.5
+
+    this.agentFieldTarget.value = halfValue.toFixed(0)
+
+    this.updatePercentage()
+  }
+
+  updatePercentage() {
+    const saleValue = Number(this.saleFieldTarget.value)
+    const agentValue = Number(this.agentFieldTarget.value)
+    const commValue = Number(this.commFieldTarget.value)
+
+    let bookingAgentPercentage = (agentValue / saleValue) * 100
+    if (isNaN(bookingAgentPercentage)) {
+      bookingAgentPercentage = 0
+    }
+    let bookingCommissionPercentage = (commValue / saleValue) * 100
+    if (isNaN(bookingCommissionPercentage)) {
+      bookingCommissionPercentage = 0
+    }
+
+    this.agentPercentageTarget.textContent = bookingAgentPercentage.toFixed(0) + '%'
+    this.commPercentageTarget.textContent = bookingCommissionPercentage.toFixed(0) + '%'
+  }
+}
