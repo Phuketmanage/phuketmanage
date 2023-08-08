2f948664560269dc57375ec1a0465a53092805c1
150Years <bytheair@gmail.com>
2023-08-08 11:39:45 +0700

    fix(pages): exclude houses whe rooms is nil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
     @houses = House.where(unavailable: false).order('random()')
     @min_date = @search.min_date
     @locations = Location.all
-    @bdrs = House.select(:rooms).distinct.pluck(:rooms).sort
+    @bdrs = House.where.not(rooms: nil).select(:rooms).distinct.pluck(:rooms).sort
     @types = HouseType.all
   end
 
