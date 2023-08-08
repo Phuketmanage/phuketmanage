62cb1cea320a46edc2d857d7c311d5828332037c
Pavel Davydov <69132749+the1723@users.noreply.github.com>
2023-08-08 15:54:42 +0300

    test(pages): homepage loads correctly (#388)

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Pages" do
+  before do
+    create(:house, :with_seasons)
+  end
+
+  describe "GET root page" do
+    it "returns http success" do
+      get '/'
+      expect(response).to have_http_status(:success)
+    end
+
+    context 'when there is house with nil rooms' do
+      before do
+        create(:house, :with_seasons, rooms: nil)
+      end
+
+      it "returns http success" do
+        get '/'
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe "GET root page with locale" do
+    it "returns http success with :ru locale" do
+      get '/ru'
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http success with :en locale" do
+      get '/en'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
