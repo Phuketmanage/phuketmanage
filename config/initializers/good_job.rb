c8c39bb9fed4101c3968f33a79fd9848e4150ac9
the1723 <hello@paveldavydov.com>
2023-08-10 15:22:49 +0300

    chore(storage): cron job time is set to run every day at 00:00 (Bangkok time)

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # Cron jobs
   config.good_job.cron = {
     storage_maintenance: { # each recurring job must have a unique key
-      cron: "5 0 * * *", # cron-style scheduling format by fugit gem
+      cron: "0 0 * * *", # Every day at 00:00 (Bangkok time) cron-style scheduling format by fugit gem
       class: "ActiveStorageMaintenanceJob", # reference the Job class with a string
       set: { priority: 10 }, # additional ActiveJob properties; can also be a lambda/proc e.g. `-> { { priority: [1,2].sample } }`
       description: "Purges unattached Active Storage blobs." # optional description that appears in Dashboard
