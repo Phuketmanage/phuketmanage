3ca21dcd9f457d1e21fb71925b865127f9077700
the1723 <hello@paveldavydov.com>
2023-08-15 12:50:59 +0300

    test(guests): routing and request specs

--- spec/requests/guests/about_spec.rb
+++ spec/requests/guests/about_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Guests::Abouts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Guests::About" do
+  describe "index" do
+    it "returns http success" do
+      get '/about'
+      expect(response).to have_http_status(:success)
+    end
+
+    context 'with locale' do
+      it "returns http success" do
+        get '/ru/about'
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
--- spec/requests/guests/index_spec.rb
+++ spec/requests/guests/index_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "Guests::Indices", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Guests::Index" do
+  before do
+    create(:house, :with_seasons)
+  end
+
+  describe "index" do
+    it "returns http success" do
+      get '/'
+      expect(response).to have_http_status(:success)
+    end
+
+    context 'with locale' do
+      it "returns http success" do
+        get '/ru'
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when there is house with nil rooms' do
+      before do
+        create(:house, :with_seasons, rooms: nil)
+      end
+
+      it "returns http success" do
+        get '/'
+        expect(response).to have_http_status(:success)
+      end
+    end
   end
 end
