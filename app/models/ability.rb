07d4ee3a07280d2e9efa3e7de4f1fc0bbea35677
150Years <bytheair@gmail.com>
2023-08-28 15:39:07 +0700

    fix(ability): acc-can-access-trasactions-docs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -33,7 +33,7 @@ class Ability
       can :statement, :document
       can %i[index edit], House
       can %i[index show canceled timeline timeline_data], Booking
-      can %i[index new create update_invoice_ref], Transaction
+      can %i[index new create update_invoice_ref docs], Transaction
       can [:edit, :update], Transaction do |t|
         t.date >= (Date.current - 30.days).beginning_of_month
       end
