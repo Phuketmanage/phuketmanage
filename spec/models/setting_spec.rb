75fb7790814f0a41572f9e318ac5a1f571f4a188
eldar88e <em88el@gmail.com>
2023-09-25 21:58:36 +0300

    test(Setting): add get_usd_rate test for the model

---
+++ spec/models/setting_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Setting do
+  context 'when call .get_usd_rate' do
+    it do
+      stub_request(:get, "https://openexchangerates.org/api/latest.json?app_id=#{ENV['YOUR_APP_ID']}")
+        .to_return(status: 200,
+                   body: '{ "disclaimer": "Usage subject to terms: https://openexchangerates.org/terms",
+                            "license": "https://openexchangerates.org/license",
+                            "timestamp": 1695650400, "base": "USD","rates": { "AED": 3.673005, "THB": 36.123 } }',
+                   headers: { 'Content-Type': 'application/json' })
+
+      usd_rate = described_class.get_usd_rate
+
+      expect(usd_rate).to be_an_instance_of(Integer)
+    end
+  end
+end
