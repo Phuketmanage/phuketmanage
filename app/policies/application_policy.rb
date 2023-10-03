e4bcd2a3d0b718311cd3efc0dc6e6678cbe56a94
Anatoly Lapkov <a.lapkov@gmail.com>
2023-10-04 06:48:19 +1200

    chore(access control): optimize code
    Co-authored-by: Pavel Davydov <69132749+the1723@users.noreply.github.com>

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -2,6 +2,34 @@ class ApplicationPolicy < ActionPolicy::Base
   authorize :user, optional: true
   pre_check :allow_admins
 
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
   private
 
     def allow_admins
