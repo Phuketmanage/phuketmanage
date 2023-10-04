bd26359e7a3adbc9812a46d66ebca6662bc34bf5
Eldar Eminov <em88el@gmail.com>
2023-10-05 01:30:17 +0300

    test(ExchangeRateJob) updated and added tests

--- spec/jobs/exchange_rate_job_spec.rb
+++ spec/jobs/exchange_rate_job_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ExchangeRateJob, type: :job do
+
   describe '#perform' do
     it 'calls Setting.get_usd_rate' do
-      expect(Setting).to receive(:get_usd_rate)
-      described_class.perform_now
+      expect(Setting).to receive(:sync_usd_rate)
+      subject.perform
     end
   end
 
-  describe '.send_email_to_admin' do
-    it 'sends an email to the admin' do
-      exception = StandardError.new('Something went wrong')
-      expect(AdminMailer).to receive(:notify_failure).with(exception).and_return(double(deliver_now: true))
-      described_class.send(:send_email_to_admin, exception)
+  describe '#retry_on' do
+    it 'retries on StandardError' do
+      allow(subject).to receive(:perform).and_raise(StandardError)
+      expect(subject).to receive(:retry_job)
+      subject.perform_now
     end
-  end
 
-  describe 'retry_on' do
-    let(:job) { described_class.new }
-
-    it 'retries on StandardError with wait time of 10 minutes and 2 attempts' do
-      #expect(described_class.retry_on_exceptions).to eq([StandardError])\
-      #expect(ExchangeRateJob.attempts).to eq(10.minutes)
-      #expect(described_class.retry_attempts).to eq(2)
+    describe '.send_email_to_admin' do
+      it 'sends an email to the admin' do
+        error = 'Something went wrong'
+        expect(AdminMailer).to receive(:notify_failure).with(error).and_return(double(deliver_now: true))
+        described_class.send(:send_email_to_admin, error)
+      end
     end
   end
 end
