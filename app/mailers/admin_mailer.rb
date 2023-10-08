ca03e5500db34bf6e794e52f2724bd7430c5e87e
Eldar Eminov <em88el@gmail.com>
2023-10-08 07:50:49 +0300

    feat(initializer) added APP_ID check for Money_exchange, which will only run when Good_job is started

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,8 @@
 class AdminMailer < ApplicationMailer
+  skip_before_action :set_settings
+
   def notify_failure(exception, subject)
     @exception = exception
-    mail(to: 'info@phuketmanage.com ', subject: )
+    mail(to: 'info@phuketmanage.com ', subject: subject)
   end
 end
--- spec/factories/job_types.rb
+++ spec/factories/job_types.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: job_types
+#
+#  id             :bigint           not null, primary key
+#  code           :string
+#  color          :string
+#  for_house_only :boolean
+#  name           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 FactoryBot.define do
   factory :job_type do
     
