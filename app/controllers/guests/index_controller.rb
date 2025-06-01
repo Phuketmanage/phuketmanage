237a31e102796446b9d8e8f4692de6caa6c1f41a
Sergey Lebedev <bytheair@gmail.com>
2025-06-01 14:35:19 +0300

    Update ruby from 3.2.2 to 3.3.0

--- app/controllers/guests/index_controller.rb
+++ app/controllers/guests/index_controller.rb
@@ -2,7 +2,7 @@ class Guests::IndexController < GuestsController
   # @route GET / (root)
   # @route GET /(:locale) (guests_locale_root)
   def index
-    # redirect_to new_user_session_path unless Rails.env.test?
+    redirect_to new_user_session_path unless Rails.env.test?
     @search = Search.new
     @min_date = @search.min_date
     @houses = House.includes(:type, :prices, :locations).where(unavailable: false).order('random()').load
