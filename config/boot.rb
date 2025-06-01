4fe53c28769caea392baf2875df7012c3f20f3eb
Sergey Lebedev <bytheair@gmail.com>
2025-06-01 10:27:36 +0300

    Set up enviroment and start project

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,10 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require_relative "boot"
+require "logger"
+require "bundler/setup"
+require "rails"
+require "bootsnap/setup"
+
+# require "bundler/setup" # Set up gems listed in the Gemfile.
+# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
