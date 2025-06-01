1e4b9e022e41355c868e0f73556d8dfa9a94cfa1
Sergey Lebedev <bytheair@gmail.com>
2025-06-01 19:22:33 +0300

    Chusaku make less strict

--- app/controllers/admin/admin_houses_controller.rb
+++ app/controllers/admin/admin_houses_controller.rb
@@ -111,6 +111,7 @@ class Admin::AdminHousesController < AdminController
       format.json { head :no_content }
     end
   end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
